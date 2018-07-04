1da3b32c81c1178b993d389415a2d85301c489c8
facundo <fluzko@gmail.com>
2018-07-04 16:23:53 -0300

    crear usuario maqueta simple terminada + abm store user

--- src/app/components/back-end/user/store/store.component.ts
+++ src/app/components/back-end/user/store/store.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../../../../services/back-end/user.service';
+import { User } from '../../../../model/user';
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
@@ -7,13 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreComponent implements OnInit {
 
-  constructor() { }
+  objectUser = new User();
+  userList: any[];
+  position: any[];
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getUser()
+      .snapshotChanges()
+      .subscribe(item => {
+        this.userList = [];
+        item.forEach(element => {
+          let x = element.payload.toJSON();
+          x["$key"] = element.key;
+          this.userList.push(x);
+        });
+      })
+      this.userService.getPosition()
+      .snapshotChanges()
+      .subscribe(item => {
+        let aux = 1;
+        this.position = [];
+        item.forEach(element => {
+          let x = element.payload.toJSON();
+           x["$key"] = element.key;
+          this.position.push(x);
 
+        });
+      })
   }
 
-
+  handleSendUser(){
+    this.userService.insertUser(this.objectUser);
+  }
 
 
 }
--- src/app/services/back-end/user.service.ts
+++ src/app/services/back-end/user.service.ts
@@ -3,22 +3,30 @@ import { Injectable } from '@angular/core';
 // FireBase
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { User } from '../../model/user';
+import { Position } from '../../model/position';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  listPosition: AngularFireList<any>;
   listUser: AngularFireList<any>;
 
   constructor( private fireBase: AngularFireDatabase ) { }
+
+  getPosition() {
+    return this.listPosition = this.fireBase.list('position');
+  }
 
   getUser() {
     return this.listUser = this.fireBase.list('user');
   }
 
+
   insertUser(userObject: User) {
     this.listUser.push ({
       name: userObject.user,
+      position: userObject.position,
       password: userObject.password
     });
   }
